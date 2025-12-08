5a4b018e2b5277e7f1d894c06f71202ddc0f1fb9
Dulis-Samantha <dulyssamantha.auteur@gmail.com>
2025-12-08 10:31:32 +0100

    Update sac-jauge.js

--- js/sac-jauge.js
+++ js/sac-jauge.js
@@ -68,7 +68,7 @@ const ITEMS = {
   'hegaia_pierre'              : { name: "Pierre Luminescente",                img: BASE_IMG + 'ing_hegaia.webp' },
   'pans_poussiere'             : { name: "Poussière d’étoile",                 img: BASE_IMG + 'ing_pan.webp' },
   'remede_miracle'             : { name: "Remède miracle",                     img: BASE_IMG + 'ing_quacks.webp' },
-  'plante_sirene'              : { name: "Plante des fonds marins",            img: BASE_IMG + 'ing_sirene_2.webp' },
+  'plante_sirene'              : { name: "Plante des fonds marins",            img: BASE_IMG + 'ing_sirene2.webp' },
   'huitre_sirene'              : { name: "Huître",                             img: BASE_IMG + 'ing_sirene.webp' },
   'arenyth_barbabichon'        : { name: "Fleur Barbabichon",                  img: BASE_IMG + 'ing_arenyth.webp' },
   'yakkas_cendre'              : { name: "Cendre de Dragon",                   img: BASE_IMG + 'ing_yakkas.webp' }
